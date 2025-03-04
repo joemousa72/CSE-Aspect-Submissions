c70ee29ea91458336d67f1f892eb2a5710226e5d
joemousa72 <yousifmousa72@gmail.com>
2025-03-04 23:29:50 +0300

    controller class add

--- Lab1/src/main/java/com/example/demo/controllers/controller.java
+++ Lab1/src/main/java/com/example/demo/controllers/controller.java
@@ -1,4 +1,26 @@
 package com.example.demo.controllers;
-@Restcontroller
-public class controller {
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class controller {  // Class name should start with a capital letter
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @DeleteMapping("/hello")
+    public String sayDelete() {
+        return "Delete";
+    }
+
+    @PostMapping("/hello")
+    public String sayPost() {
+        return "Post";
+    }
+
+    @PutMapping("/hello")
+    public String sayPut() {
+        return "Put";
+    }
 }
